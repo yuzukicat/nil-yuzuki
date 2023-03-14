5857f8d04101c8168d1a4b5f337bae5f77a340eb
yuzukicat <yuzuki.cat@kamisu66.com>
2023-03-14 12:29:37 +0900

    penguin_info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn greet_world(){
+fn _greet_world(){
     let southern_germany = "GE";
     let chinese = "ni hao";
     let english = "hello world";
@@ -8,6 +8,36 @@ fn greet_world(){
     }
 }
 
+fn penguin_info() {
+    // data to be handled
+   let penguin_data = "\
+   common name,length (cm)
+   Little penguin,33
+   Yellow-eyed penguin,65
+   Fiordland penguin,60
+   Invalid,data
+   ";
+    let records = penguin_data.lines();
+    // println!("{}", penguin_data);
+    // println!("{}", &penguin_data)
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+        // use elastic array
+        // it is record oriented programming
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
+        if cfg!(debug_assertiions) {
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+        let common_name = fields[0];
+        if let Ok(height) = fields[1].parse::<f32>() {
+            println!("{}, {}cm", common_name, height)
+        }
+    }
+}
+
 fn main() {
-    greet_world();
+    // greet_world();
+    penguin_info();
 }
