7c40411522574c33437251afd2bb4221ad50d5cd
yuzukicat <yuzuki.cat@kamisu66.com>
2023-03-29 14:44:12 +0900

    rust num experiment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn _greet_world(){
+fn _greet_world() {
     let southern_germany = "GE";
     let chinese = "ni hao";
     let english = "hello world";
@@ -10,7 +10,7 @@ fn _greet_world(){
 
 fn _penguin_info() {
     // data to be handled
-   let penguin_data = "\
+    let penguin_data = "\
    common name,length (cm)
    Little penguin,33
    Yellow-eyed penguin,65
@@ -37,7 +37,7 @@ fn _penguin_info() {
     }
 }
 
-fn _print_boolean(){
+fn _print_boolean() {
     let (a, mut b) = (true, false);
     println!("{:?}, {:?}", a, b);
 
@@ -45,21 +45,69 @@ fn _print_boolean(){
     assert_eq!(a, b);
 }
 
-struct Struct {
-    e: i32
+// struct Struct {
+//     e: i32,
+// }
+
+// fn match_mode() {
+//     let (a, b, c, d, e);
+//     (a, b) = (1, 2);
+//     [c, .., d, _] = [1, 2, 3, 4, 5];
+//     Struct { e } = Struct { e: 5 };
+//     assert_eq!([1, 2, 1, 4, 5], [a, b, c, d, e]);
+// }
+
+fn _warpping_handling() {
+    let a: u8 = 255;
+    let b = a.wrapping_add(20);
+    println!("{}", b);
 }
 
-fn match_mode() {
-    let (a, b, c, d, e);
-    (a, b) = (1, 2);
-    [c, .., d, _] = [1, 2, 3, 4, 5];
-    Struct { e,  } = Struct { e: 5};
-    assert_eq!([1, 2, 1, 4, 5], [a, b, c, d, e]);
+fn _floating_experiment() {
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+    println!("abc (f32)");
+    println!("    0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("          0.3: {:x}", (abc.2).to_bits());
+    println!();
+    println!("xyz (f64)");
+    println!("    0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("          0.3: {:x}", (xyz.2).to_bits());
+    println!();
+    assert!(abc.0 + abc.1 == abc.2);
+    assert!(xyz.0 + xyz.1 == xyz.2);
+}
+
+fn _nan_experiment() {
+    let x = (-42_f32).sqrt();
+    // assert_eq!(x, x);
+    if x.is_nan() {
+        println!("undefined math operation")
+    }
+}
+
+fn _cal_experiment() {
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+    let addition = twenty + twenty_one + twenty_two;
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+    let forty_twos = [42.0, 42f32, 42.0f32];
+    println!("{:.2}", forty_twos[0]);
 }
 
 fn main() {
     // greet_world();
     // penguin_info();
     // print_boolean();
-    match_mode();
+    // match_mode();
+    // warpping_handling();
+    // floating_experiment();
+    // nan_experiment();
+    // cal_experiment();
 }
